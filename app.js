b121e971ceb63450041a756325fcb7065aafd20d
shivamkonkar <shivamkonkar25@gmail.com>
2023-05-28 17:56:00 +0530

    firebase deployement

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.post("/", function(req, res){
     })
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started http://localhost:3000/");
 })
